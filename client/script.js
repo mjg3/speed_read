368fdc4ece0874d913318c6e3f333495f0d0c1e1
mjg3 <mjg3@princeton.edu>
2015-11-20 17:58:52 -0800

    Organized front-end angular folder.
    Organized the angular front end folders

--- client/script.js
+++ client/script.js
@@ -1,20 +1,10 @@
-var store_app = angular.module('store_app', ['ngRoute']);
+var reading_app = angular.module('reading_app', ['ngRoute']);
 
-store_app.config(function($routeProvider){
-  $routeProvider
-
-  .when('/', {
-    templateUrl: '/partials/flash_cards.html'
-  })
-
-})
-
-
-store_app.factory('Factory', function($http){
+reading_app.factory('Factory', function($http){
 });
 
 
-store_app.controller('Controller', function($scope, customerFactory){
+reading_app.controller('Controller', function($scope, customerFactory){
   $scope.practice_string = "I am working at the Coding Dojo right now!";
 
 })
