4aeba38135ff69371822b0554b949506125fddb5
mjg3 <mjg3@princeton.edu>
2015-11-26 16:47:17 -0700

    Deploy

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var path = require('path');
 var app  = express();
 var bodyParser = require('body-parser');
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8000;
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
 
@@ -13,6 +17,10 @@ require('./server/config/mongoose.js');
 // routes is probably the last thing to be loaded before the server
 require('./server/config/routes.js')(app);
 
-app.listen(8000, function(){
-  console.log("Listening on port 8000");
-})
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+// app.listen(8000, function(){
+//   console.log("Listening on port 8000");
+// })
