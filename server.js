d2c0b1f5943a2ba6ee995b0791a0bbfb060ad974
mjg3 <mjg3@princeton.edu>
2015-12-07 12:40:16 -0800

    Cut passport features

--- server.js
+++ server.js
@@ -4,25 +4,11 @@ var path = require('path');
 var app  = express();
 var bodyParser = require('body-parser');
 
-var passport = require('passport');
 
-// This is the file we created in step 2.
-// This will configure Passport to use Auth0
-var strategy = require('./server/controllers/setup-passport');
-
-// Session and cookies middlewares to keep user logged in
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
-app.use(cookieParser());
-app.use(session({ secret: 'shhhhhhhhh' }));
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 require('./server/config/mongoose.js');
